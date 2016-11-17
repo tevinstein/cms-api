1468a53a46390fda3ea001989036056110a9e530
tevinstein <tevinsa@yahoo.com>
2016-11-17 16:11:18 +0700

    checkpoint 2

---
+++ public/javascript/script.datadates.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+    getAllDataDates()
+    $('#add-data-date').hide()
+    $(function() {
+        $("#datepicker").datepicker();
+        $("#datepicker2").datepicker();
+    })
+    searchDataDates()
+})
+
+function searchDataDates() {
+    $('input[name=search-data-date-letter], input[name=search-data-date-frequency]').on('keyup', function() {
+        getAllDataDates()
+    })
+}
+
+function getAllDataDates() {
+    $.ajax({
+        url: "/api/datadates",
+        type: 'GET',
+        data: {
+            letter: $("input[name=search-data-date-letter]").val(),
+            frequency: $("input[name=search-data-date-frequency]").val()
+        },
+        dataType: 'json',
+        success: function(results) {
+            listDataDate(results)
+        }
+    })
+}
+
+function listDataDate(results) {
+    $('.table-data-date tbody').empty()
+
+    for (var i = 0; i < results.length; i++) {
+        $('.table-data-date tbody').append('<tr><td>' + (i + 1) + '</td><td class="data-date-letter">' + results[i].letter + '</td><td class="data-date-frequency">' + results[i].frequency + '</td><td><button onclick="editDataDate(this,event)" data-id="' + results[i]._id + '" class="btn btn-sm btn-default"><span class="fa fa-pencil"></span></button><button onclick="deleteDataDate(this)" class="btn btn-sm btn-default" data-id="' + results[i]._id + '"><span class="fa fa-trash"></span></button></td></tr>')
+    }
+}
+
+function addDataDate(event) {
+    event.preventDefault()
+
+    var id = $('#add-data-date-button').attr('data-id')
+
+    if ($('#add-data-date-button').attr('data-id')) {
+        $.ajax({
+            url: "/api/datadates/" + id,
+            type: 'PUT',
+            data: { letter: $("input[name=data-date-letter]").val(), frequency: $("input[name=data-date-frequency]").val() },
+            success: function() {
+                $('#data-date-message').empty()
+                $('#data-date-message').append('<div class="alert alert-warning">Data date is edited!</div>')
+                $('#add-data-date-button').removeAttr('data-id');
+                getAllDataDates()
+
+                $("input[name=data-date-letter]").val('');
+                $("input[name=data-date-frequency]").val('');
+            }
+        })
+    } else {
+        $.ajax({
+            url: "/api/datadates",
+            type: 'POST',
+            data: { letter: $("input[name=data-date-letter]").val(), frequency: $("input[name=data-date-frequency]").val() },
+            success: function() {
+                $('#data-date-message').empty()
+                $('#data-date-message').append('<div class="alert alert-warning">Data date is added!</div>')
+                $('#add-data-date-button').removeAttr('data-id');
+                getAllDataDates()
+
+                $("input[name=data-date-letter]").val('');
+                $("input[name=data-date-frequency]").val('');
+            }
+        })
+    }
+}
+
+function editDataDate(pointer, event) {
+    var id = $(pointer).attr('data-id')
+    var letter = $(pointer).closest('tr').find('.data-date-letter').text()
+    var frequency = $(pointer).closest('tr').find('.data-date-frequency').text()
+
+    $("input[name=data-date-letter]").val(letter);
+    $("input[name=data-date-frequency]").val(frequency);
+
+    $('#add-data-date-button').attr('data-id', id)
+    $('#add-data-date').show()
+}
+
+function deleteDataDate(pointer) {
+    var id = $(pointer).attr('data-id')
+    $.ajax({
+        url: "/api/datadates/" + id,
+        type: 'DELETE',
+        success: function() {
+            $(pointer).closest('tr').remove()
+            $('#data-date-message').empty()
+            $('#data-date-message').append('<div class="alert alert-warning">Data is deleted</div>')
+        }
+    })
+}
+
+function showAddDataDate() {
+    $('#add-data-date').toggle()
+    $('#data-date-message').empty()
+    if ($('#add-data-date-button').attr('data-id')) {
+        $('#add-data-date-button').removeAttr('data-id');
+    }
+}
---
+++ public/javascript/script.datas.js
@@ -0,0 +1,117 @@
+$(document).ready(function() {
+    getAllDatas()
+    $('#add-data').hide()
+    searchDatas()
+})
+
+function searchDatas() {
+    $('input[name=search-data-letter], input[name=search-data-frequency]').on('keyup', function() {
+        getAllDatas()
+    })
+}
+
+function getAllDatas() {
+    $.ajax({
+        url: "/api/datas",
+        type: 'GET',
+        data: {
+            letter: $("input[name=search-data-letter]").val().toUpperCase(),
+            frequency: $("input[name=search-data-frequency]").val()
+        },
+        dataType: 'json',
+        success: function(results) {
+            listData(results)
+        }
+    })
+}
+
+function listData(results) {
+    $('.table-data tbody').empty()
+
+    for (var i = 0; i < results.length; i++) {
+        $('.table-data tbody').append('\
+            <tr>\
+                <td>' + (i + 1) + '</td>\
+                <td class="data-letter">' + results[i].letter + '</td>\
+                <td class="data-frequency">' + results[i].frequency + '</td>\
+                <td>\
+                    <button onclick="editData(this,event)" id="edit-data-button" data-id="' + results[i]._id + '" class="btn btn-sm btn-default"><span class="fa fa-pencil"></span></button>\
+                    <button id="delete-data-button" class="btn btn-sm btn-default" data-toggle="modal" data-target="#myModal" data-id="' + results[i]._id + '"><span class="fa fa-trash"></span></button>\
+                </td>\
+            </tr>\
+            ')
+    }
+}
+
+function addData(event) {
+    event.preventDefault()
+    var id = $('#add-data-button').attr('data-id')
+
+    if ($('#add-data-button').attr('data-id')) {
+        $.ajax({
+            url: "/api/datas/" + id,
+            type: 'PUT',
+            data: { letter: $("input[name=data-letter]").val().toUpperCase(), frequency: $("input[name=data-frequency]").val() },
+            success: function() {
+                $('#data-message').empty()
+                $('#data-message').append('<div class="alert alert-info">Data is edited!</div>')
+                $('#add-data-button').removeAttr('data-id');
+                getAllDatas()
+
+                $("input[name=data-letter]").val('');
+                $("input[name=data-frequency]").val('');
+
+            }
+        })
+    } else {
+        $.ajax({
+            url: "/api/datas",
+            type: 'POST',
+            data: { letter: $("input[name=data-letter]").val().toUpperCase(), frequency: $("input[name=data-frequency]").val() },
+            success: function() {
+                $('#data-message').empty()
+                $('#data-message').append('<div class="alert alert-info">Data is added!</div>')
+                $('#add-data-button').removeAttr('data-id');
+
+                getAllDatas()
+                $("input[name=data-letter]").val('');
+                $("input[name=data-frequency]").val('');
+            }
+        })
+    }
+}
+
+function editData(pointer,event) {
+    var id = $(pointer).attr('data-id')
+    var letter = $(pointer).closest('tr').find('.data-letter').text()
+    var frequency = $(pointer).closest('tr').find('.data-frequency').text()
+
+    $("input[name=data-letter]").val(letter);
+    $("input[name=data-frequency]").val(frequency);
+
+    $('#add-data-button').attr('data-id', id)
+    $('#add-data').show()
+}
+
+function deleteData(pointer,event) {
+    event.preventDefault()
+
+    var id = $(pointer).attr('data-id')
+    $.ajax({
+        url: "/api/datas/" + id,
+        type: 'DELETE',
+        success: function() {
+            $(pointer).closest('tr').remove()
+            $('#data-message').empty()
+            $('#data-message').append('<div class="alert alert-info">Data is deleted</div>')
+        }
+    })
+}
+
+function showAddData() {
+    $('#add-data').toggle()
+    $('#data-message').empty()
+    if ($('#add-data-button').attr('data-id')) {
+        $('#add-data-button').removeAttr('data-id');
+    }
+}
